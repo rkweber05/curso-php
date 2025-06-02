98a2ee3b2a2b24f9185aae4a7543bf6e2b784e91
Rodrigo Kunzler Weber <rkweber05@outlook.com>
2025-06-02 09:31:49 -0300

    feat: Primitive types

---
+++ ex003/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos primitivos em PHP</title>
+</head>
+<body>
+    <h1>Testes de tipos primitivos</h1>
+
+    <?php 
+        // 0x = hexadecimal 0b = binário 0 = octal
+        // $num = 10;
+        // echo "O valor da variável é $num";
+
+        // $v = "Rodrigo";
+        // var_dump($v);
+
+        // $casado = true;
+        // var_dump($casado);
+        // print "O valor para casado é $casado"
+
+        // $vet = [6, 2.5, "Maria", 3, false];
+        // var_dump($vet);
+    
+        class Pessoa {
+            private string $name;
+        }
+
+        $p = new Pessoa;
+    ?>
+</body>
+</html>
